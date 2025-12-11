0d170458f2258b54398bf930ca33224795accec1
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-12-11 23:57:30 +0530

    Update API baseURL to remove local fallback

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://sgims-backend.onrender.com/" || "http://127.0.0.1:5000/api",
+  baseURL: "https://sgims-backend.onrender.com",
 });
 
 // Always attach token
