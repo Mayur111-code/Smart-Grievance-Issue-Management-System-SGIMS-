cfa1775f6cac9d3d3b45b15b7cde161e4ada313b
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-12-12 01:09:30 +0530

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://sgims-backend.onrender.com",
+  baseURL: "https://sgims-backend.onrender.com/api",
 });
 
 // Always attach token
